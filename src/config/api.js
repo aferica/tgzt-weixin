1070d731aec5fff36f3dac88228cbe934bfa15aa
wangfeizhou <18819466397@163.com>
2019-05-15 09:23:44 +0800

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,11 @@
+export default {
+  // host: 'http://182.254.164.22:8080',
+  // host: 'http://127.0.0.1:8080',
+  host: 'https://www.dichanzhaopin.com',
+
+  login: '/tgcareer/sys/login',
+  user: '/tgcareer/user/user',
+  salary: '/tgcareer/salary/salary',
+  city: '/tgcareer/user/city',
+  look: '/tgcareer/user/looks'
+}
